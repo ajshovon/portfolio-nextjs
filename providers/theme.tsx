eac1b79aa2e8ed24b69165a4726862b623f28a4a
Jubayer <jubayeras382@outlook.com>
2024-08-27 17:18:01 +0600

    set theme with query
    Signed-off-by: Jubayer <jubayeras382@outlook.com>

--- providers/theme.tsx
+++ providers/theme.tsx
@@ -1,10 +1,14 @@
-"use client";
+'use client';
 
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from 'next-themes';
+
+import { useSearchParams } from 'next/navigation';
 
 export function SiteThemeProvider({ children }: { children: React.ReactNode }) {
+  const searchParams = useSearchParams();
+  const theme = searchParams.get('theme');
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme={theme ? theme : 'system'} enableSystem>
       {children}
     </ThemeProvider>
   );
