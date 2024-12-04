965a1429e860742d3393dee0514c7274a7911d7f
Jubayer <jubayeras382@outlook.com>
2024-12-04 12:03:14 +0600

    fix: update project details and improve formatting in ProjectsPage and ProjectElement components
    Signed-off-by: Jubayer <jubayeras382@outlook.com>

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,48 +1,69 @@
-import ProjectElement from "@/components/ui/ProjectElement";
-import type { Metadata } from "next";
-import React from "react";
+import ProjectElement from '@/components/ui/ProjectElement';
+import type { Metadata } from 'next';
+import React from 'react';
 
 export const metadata: Metadata = {
-  title: "Projects :: [.shovon]",
-  description: "Personal Portfolio || [.shovon]",
+  title: 'Projects :: [.shovon]',
+  description: 'Personal Portfolio || [.shovon]',
 };
 
 const ProjectsPage: React.FC = () => {
   const projects = [
     {
       id: 1,
-      name: "PitchBlack Recovery Project",
-      description:
-        "PitchBlack Recovery Project is an open source custom recovery for android by developers from different countries with an aim to provide an advanced recovery with better customizations, themes and features.",
-      image: "/assets/images/PitchBlackRecoveryProject.png",
-      live: "https://pitchblackrecovery.com/",
-      source: "https://github.com/PitchBlackRecoveryProject",
+      name: 'PitchBlack Recovery Project',
+      description: 'PitchBlack Recovery Project is an open source custom recovery for android by developers from different countries with an aim to provide an advanced recovery with better customizations, themes and features.',
+      image: '/assets/images/PitchBlackRecoveryProject.png',
+      live: 'https://pitchblackrecovery.com/',
+      source: 'https://github.com/PitchBlackRecoveryProject',
     },
     {
       id: 2,
-      name: "Typenspeed",
-      description: "A Django based web app to test your typing speed!",
-      image: "/assets/images/tns.svg",
-      live: "https://typenspeed.ml/",
-      source: "https://github.com/enigma71/type-n-speed",
+      name: 'DASH ⚡',
+      description: 'DASH (Distributed Address Shortening Hub) is a decentralized application designed to shorten URLs. It leverages Cloudflare network to ensure fast and reliable short links.',
+      image: '/assets/images/dash.png',
+      live: 'https://dash-demo.shovon.me/login',
+      source: 'https://re.shovon.me/dash-code',
     },
     {
       id: 3,
-      name: "Registro",
-      description:
-        "A simple web app made with flask for managing student's registration & courses information.",
-      image: "/assets/images/pyrunner.png",
-      live: "",
-      source: "https://github.com/shovon382/registro",
+      name: '.ask',
+      description: ".ask is an anonymous question-asking platform similar to NGL, designed to be self-hosted for maximum control and privacy. It leverages Cloudflare's network for fast and reliable performance, ensuring that your platform is always accessible.",
+      image: '/assets/images/ask.png',
+      live: 'https://ask.shovon.me',
+      source: 'https://re.shovon.me/ask-code',
     },
     {
       id: 4,
-      name: "pyRunner",
-      description:
-        "A telegram bot to run python codes. By providing a platform for running Python code within Telegram, this bot facilitates quick testing, learning, and experimenting with Python without the need for a separate development environment.",
-      image: "/assets/images/pyrunner.png",
-      live: "",
-      source: "https://github.com/shovon382/pyrunner",
+      name: 'CGPA Calculator',
+      description: 'A Responsive Web Interface for Calculating CGPA or Cumulative Grade Point Average of DIU Students.',
+      image: '/assets/images/cgpa.png',
+      live: 'https://diu-cgpa.shovon.me',
+      source: 'https://redirect.shovon.me/diu-cgpa-github',
+    },
+    {
+      id: 5,
+      name: 'Typenspeed',
+      description: 'A Django based web app to test your typing speed!',
+      image: '/assets/images/tns.svg',
+      live: 'https://typenspeed.ml/',
+      source: 'https://github.com/enigma71/type-n-speed',
+    },
+    {
+      id: 6,
+      name: 'Registro',
+      description: "A simple web app made with flask for managing student's registration & courses information.",
+      image: '/assets/images/pyrunner.png',
+      live: '',
+      source: 'https://github.com/shovon382/registro',
+    },
+    {
+      id: 7,
+      name: 'pyRunner',
+      description: 'A telegram bot to run python codes. By providing a platform for running Python code within Telegram, this bot facilitates quick testing, learning, and experimenting with Python without the need for a separate development environment.',
+      image: '/assets/images/pyrunner.png',
+      live: '',
+      source: 'https://github.com/shovon382/pyrunner',
     },
   ];
   return (
@@ -53,16 +74,7 @@ const ProjectsPage: React.FC = () => {
         </div>
         <div className="flex flex-col divide-y dark:divide-[#363839] divide-gray-300">
           {projects.map((project) => {
-            return (
-              <ProjectElement
-                key={project.id}
-                title={project.name}
-                description={project.description}
-                image={project.image}
-                live={project.live}
-                source={project.source}
-              />
-            );
+            return <ProjectElement key={project.id} title={project.name} description={project.description} image={project.image} live={project.live} source={project.source} />;
           })}
         </div>
       </div>
